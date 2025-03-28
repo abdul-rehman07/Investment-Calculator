5c1943d2bef0bc9bbec210077c076464ad0a26e2
abdulrehman <kiyanimani777@mail.com>
2025-03-28 15:30:53 +0500

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useState } from "react";
+import Header from "./assets/Components/Header"
+import UserInput from "./assets/Components/UserInput"
+import Result from "./assets/Components/Result";
+function App() {
+  const [userInput, setUserInput] = useState(
+    {
+      initialInvestment: 10000,
+      annualInvestment: 1200,
+      expectedReturn: 6,
+      duration: 10,
+    }
+  );
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      }
+    })
+  }
+
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && <p className="center">Please Enter A Duration Greater Then ZERO </p>}
+      {inputIsValid && <Result input={userInput} />}
+    </>
+  )
+}
+
+export default App;
